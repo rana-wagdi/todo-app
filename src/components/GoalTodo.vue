94ea02991adfdfe5de5a84f954107069b6bc107d
Rana Wagdi <>
2022-12-06 15:43:17 +0200

    initial commit

---
+++ src/components/GoalTodo.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="goal">
+    <div @click="resetTask" class="circle_goal">
+      <span class="circle_goal_item"></span>
+    </div>
+    <input
+      class="goal"
+      type="text"
+      ref="goal"
+      v-model="enterGoal"
+      placeholder="Create a new todo..."
+      @keyup.enter="$emit('addTask')"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+props:['todoLists'],
+data(){
+    return{
+        enterGoal: "",
+        todoList: this.todoLists
+    }
+},
+  methods: {
+    addTask() {
+      const enterValue = this.$refs.goal.value;
+      if (this.enterGoal.length > 0) {
+      this.todoList =this.todoList.push({
+          id: new Date().toISOString(),
+          task: enterValue,
+          completed: false
+        });
+      } else {
+        alert("Please Insert Some Goals");
+      }
+      this.enterGoal = "";
+    },
+    resetTask() {
+      this.enterGoal = "";
+    },
+  },
+};
+</script>
---
+++ src/components/TheList.vue
@@ -0,0 +1,39 @@
+<template>
+  <div :class="[isActive ? 'dark' : 'light']">
+    <div class="content">
+      <div class="header__todo"></div>
+      <div class="todo__content">
+        <header-todo @change-mode="enableLightMode"></header-todo>
+        <todo-list></todo-list>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import HeaderTodo from "./HeaderTodo.vue";
+import TodoList from "./TodoList.vue";
+
+export default {
+  props: ["modeTheme"],
+  components: {
+    HeaderTodo,
+    TodoList,
+  },
+  data() {
+    return {
+      isActive: true,
+    };
+  },
+
+  methods: {
+    themeMode() {
+      this.isActive = !this.isActive;
+      this.$emit("change-mode", this.isActive);
+    },
+    enableLightMode(isActive) {
+      this.isActive = isActive;
+    },
+  },
+};
+</script>
